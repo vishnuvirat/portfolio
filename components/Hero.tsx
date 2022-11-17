7ada7ee7b30a31cf41c090dd9583ce93fb153469
Vishnu Chiluveri <vishnuviratkohli5@gmail.com>
2022-11-17 22:40:16 +0530

    Image added

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,20 +1,33 @@
 import React from "react";
 import Typewriter from "typewriter-effect";
 import BackgroundCircle from "./BackgroundCircle";
+import Image from "next/image";
 
 type Props = {};
 
 const Hero = (props: Props) => {
   return (
     <div className="flex items-center justify-center h-screen flex-col space-y-8 text-center overflow-hidden">
-      <BackgroundCircle/>
-      <Typewriter
-        options={{
-          strings: ["Hi! Iam Vishnu", "Code Blooded", "<But Loves To Code more/>"],
-          autoStart: true,
-          loop: true,
-        }}
+      <BackgroundCircle />
+      <Image
+        className="relative rounded-full mx-auto object-cover h-32 w-32"
+        src="/vishnu.jpg"
+        height="500"
+        width="500"
       />
+      <div>
+        <Typewriter
+          options={{
+            strings: [
+              "Hi! Iam Vishnu",
+              "Code Blooded",
+              "<But Loves To Code more/>",
+            ],
+            autoStart: true,
+            loop: true,
+          }}
+        />
+      </div>
     </div>
   );
 };
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
 
+
 export default function Home() {
   return (
     <div className='bg-[rgb(36,36,36)] h-screen text-white snap-y snap-mandatory overflow-scroll z-0'>
