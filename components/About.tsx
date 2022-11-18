73778294e80c8efd0a8e67ad99d069b1a647b3a5
Vishnu Chiluveri <vishnuviratkohli5@gmail.com>
2022-11-19 00:27:32 +0530

    About completed

--- components/About.tsx
+++ components/About.tsx
@@ -1,17 +1,55 @@
-import React from 'react'
+import React from "react";
 import Image from "next/image";
 import { motion } from "framer-motion";
 
-type Props = {}
+type Props = {};
 
 const About = (props: Props) => {
   return (
-    <div className='h-screen flex flex-col md:flex-row relative text-center md:text-left max-w-7xl mx-auto justify-evenly px-10 items-center'>
-      <div className='tracking-[20px] text-gray-500 text-2xl absolute top-24 uppercase'>About</div>
-      <motion.div><Image src="./vishnu.jpg" alt='' height="500" width="500"></Image></motion.div>
-      <div></div>
+    <div className="h-screen flex flex-col md:flex-row relative text-center md:text-left max-w-7xl mx-auto justify-evenly px-10 items-center">
+      <div className="tracking-[20px] text-gray-500 text-2xl absolute top-24 uppercase">
+        About
+      </div>
+      <motion.div
+        className=""
+        initial={{
+          x: -200,
+        }}
+        transition={{
+          duration: 1.5,
+        }}
+        whileInView={{
+          opacity: 1,
+          x: 0,
+        }}
+        viewport={{ once: true }}
+      >
+        <Image
+          className="-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[400px] xl:h-[500px]"
+          src="/vishnu.jpg"
+          alt=""
+          height="1000"
+          width="1000"
+        ></Image>
+      </motion.div>
+      <div className="space-y-10 px-0 md:px-10">
+        <div className="text-2xl font-semibold md:text-3xl xl:text-5xl">
+          Here is a Little Background
+        </div>
+        <div className="text-sm md:max-w-7xl md:mx-auto">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged. It was popularised in the 1960s with
+          the release of Letraset sheets containing Lorem Ipsum passages, and
+          more recently with desktop publishing software like Aldus PageMaker
+          including versions of Lorem Ipsum.
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
