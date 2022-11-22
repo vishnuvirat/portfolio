7a5fa87bcf01263f53400b4eba25d7643f99bc04
Vishnu Chiluveri <vishnuviratkohli5@gmail.com>
2022-11-22 21:25:01 +0530

    Created skills component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,29 @@
-import Head from 'next/head'
-import About from '../components/About'
-import Header from '../components/Header'
-import Hero from '../components/Hero'
-import Experience from '../components/Experience'
-
+import Head from "next/head";
+import About from "../components/About";
+import Header from "../components/Header";
+import Hero from "../components/Hero";
+import Experience from "../components/Experience";
+import Skills from "../components/Skills";
 
 export default function Home() {
   return (
-    <div className='bg-[rgb(36,36,36)] h-screen text-white snap-y snap-mandatory overflow-scroll z-0'>
+    <div className="bg-[rgb(36,36,36)] h-screen text-white snap-y snap-mandatory overflow-scroll z-0">
       <Head>
         <title>Vishnu Portfolio</title>
       </Head>
-        <Header/>
-        <div id='hero' className='snap-start'><Hero/></div>
-        <div id='about' className='snap-center'><About/></div>
-        <div id='experience' className='snap-center'><Experience/></div>
+      <Header />
+      <div id="hero" className="snap-start">
+        <Hero />
+      </div>
+      <div id="about" className="snap-center">
+        <About />
+      </div>
+      <div id="experience" className="snap-center">
+        <Experience />
+      </div>
+      <div>
+        <Skills />
+      </div>
     </div>
-  )
+  );
 }
