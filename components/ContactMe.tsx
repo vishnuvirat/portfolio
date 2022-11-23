9ce5db657df5a468b687af5775d282e669f57d6f
Vishnu Chiluveri <vishnuviratkohli5@gmail.com>
2022-11-23 23:35:12 +0530

    ContactMe component created

---
+++ components/ContactMe.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const ContactMe = (props: Props) => {
+  return (
+    <div className='h-screen'>ContactMe</div>
+  )
+}
+
+export default ContactMe
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Hero from "../components/Hero";
 import Experience from "../components/Experience";
 import Skills from "../components/Skills";
 import Projects from "../components/Projects";
+import ContactMe from "../components/ContactMe";
 
 export default function Home() {
   return (
@@ -27,6 +28,9 @@ export default function Home() {
       </div>
       <div id="projects" className="snap-start">
         <Projects />
+      </div>
+      <div id="contact" className="snap-start">
+        <ContactMe/>
       </div>
     </div>
   );
