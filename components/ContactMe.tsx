4efecedf291437626b30bfa0d710631ffd0b38de
Vishnu Chiluveri <vishnuviratkohli5@gmail.com>
2022-11-23 23:56:21 +0530

    installed hero icons

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,11 +1,16 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = {};
 
 const ContactMe = (props: Props) => {
   return (
-    <div className='h-screen'>ContactMe</div>
-  )
-}
+    <div className="h-screen flex relative flex-col text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center">
+      <div className="uppercase tracking-[20px] text-gray-500 top-24 absolute text-2xl">Contact</div>
+      <div className="flex flex-col space-y-10">
+        <div className="text-4xl font-semibold text-center">I Have got just what you need</div>
+      </div>
+    </div>
+  );
+};
 
-export default ContactMe
+export default ContactMe;
