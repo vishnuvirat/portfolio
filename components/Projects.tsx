5bf28e6400d5b6911b5dfc3e296684507a5bcac8
Vishnu Chiluveri <vishnuviratkohli5@gmail.com>
2022-11-23 23:32:16 +0530

    Projects page completed

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Image from "next/image";
+import { motion } from "framer-motion";
 
 type Props = {};
 
@@ -7,18 +8,30 @@ const Projects = (props: Props) => {
   const projects = [1, 2, 3, 4, 5];
   return (
     <>
-      <div className="h-screen relative flex flex-col overflow-hidden text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0">
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        transition={{ duration: 1.5 }}
+        className="h-screen relative flex flex-col overflow-hidden text-left md:flex-row max-w-full justify-evenly mx-auto items-center z-0"
+      >
         <div className="absolute tracking-[20px] uppercase top-24 text-gray-500 text-2xl">
           Projects
         </div>
         <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20">
           {projects.map((project, i) => (
             <div className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
-              <Image src="/vishnu.jpg" alt="" width="200" height="200" />
+              <motion.div
+                initial={{ y: -300, opacity: 0 }}
+                whileInView={{ opacity: 1, y: 0 }}
+                transition={{ duration: 1.5 }}
+                viewport={{ once: true }}
+              >
+                <Image src="/vishnu.jpg" alt="" width="200" height="200" />
+              </motion.div>
               <div className="text-4xl font-sans font-semibold">
                 <div>Case Study {i + 1} of 0: UPS Clone </div>
               </div>
-              <div>
+              <div className="text-lg text-center md:text-left">
                 Lorem ipsum, or lipsum as it is sometimes known, is dummy text
                 used in laying out print, graphic or web designs. The passage is
                 attributed to an unknown typesetter in the 15th century who is
@@ -29,7 +42,7 @@ const Projects = (props: Props) => {
           ))}
         </div>
         <div className="w-full absolute top-[30%] bg-[#F7AB0A] left-0 h-[500px] -skew-y-12 opacity-10"></div>
-      </div>
+      </motion.div>
     </>
   );
 };
