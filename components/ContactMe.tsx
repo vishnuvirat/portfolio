de4c54da6095df1406405ec14643a8da46215380
Vishnu Chiluveri <vishnuviratkohli5@gmail.com>
2022-11-24 00:05:13 +0530

    Contact updated

--- components/ContactMe.tsx
+++ components/ContactMe.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {PhoneIcon, MapPinIcon, EnvelopeIcon} from '@heroicons/react/24/solid'
 
 type Props = {};
 
@@ -8,6 +9,10 @@ const ContactMe = (props: Props) => {
       <div className="uppercase tracking-[20px] text-gray-500 top-24 absolute text-2xl">Contact</div>
       <div className="flex flex-col space-y-10">
         <div className="text-4xl font-semibold text-center">I Have got just what you need</div>
+      </div>
+      <div className="flex items-center space-x-5 justify-center">
+        <PhoneIcon className="animate-pluse text-[#F7AB0A] h-7 w-7 "/>
+        <div className="text-2xl">+91 7995803197</div>
       </div>
     </div>
   );
