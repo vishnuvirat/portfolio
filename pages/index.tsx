c218925d9c379ee32d7c2495495d2160683677ae
Vishnu Chiluveri <vishnuviratkohli5@gmail.com>
2022-11-24 21:33:17 +0530

    Scroll bar updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ import ContactMe from "../components/ContactMe";
 
 export default function Home() {
   return (
-    <div className="bg-[rgb(36,36,36)] h-screen text-white snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0">
+    <div className="bg-[rgb(36,36,36)] h-screen text-white snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
       <Head>
         <title>Vishnu Portfolio</title>
       </Head>
@@ -30,7 +30,7 @@ export default function Home() {
         <Projects />
       </div>
       <div id="contact" className="snap-start">
-        <ContactMe/>
+        <ContactMe />
       </div>
     </div>
   );
