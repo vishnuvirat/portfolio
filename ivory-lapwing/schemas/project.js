1fc0c2c3421a4bfa8bfbce4193c07bc6361ab9d4
Vishnu Chiluveri <vishnuviratkohli5@gmail.com>
2022-11-24 23:44:54 +0530

    Project created in sanity backend

---
+++ ivory-lapwing/schemas/project.js
@@ -0,0 +1,50 @@
+export default {
+  name: "project",
+  title: "Project",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      title: "Title",
+      description: 'Title of the project',
+      type: "string",
+    },
+
+    {
+      name: "summary",
+      title: "Summary",
+      type: "string",
+    },
+
+    {
+      name: "companyImage",
+      title: "CompnayImage",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+
+    {
+      name: "image",
+      title: "Image",
+      type: "image",
+      options: {
+        hotspot: true,
+      },
+    },
+
+    {
+      name: "linkToBuild",
+      title: "LinkToBuild",
+      type: "url",
+    },
+
+    {
+      name: "technologies",
+      title: "Technologies",
+      type: "array",
+      of: [{ type: "reference", to: { type: "skill" } }],
+    },
+  ],
+};
