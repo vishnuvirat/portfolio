513cf4a5cd37765ef567e8db3f9d3860ec9569ed
Vishnu Chiluveri <vishnuviratkohli5@gmail.com>
2022-11-27 23:38:44 +0530

    Updates

--- ivory-lapwing/schemas/schema.js
+++ ivory-lapwing/schemas/schema.js
@@ -1,15 +1,17 @@
 // First, we must import the schema creator
-import createSchema from 'part:@sanity/base/schema-creator'
+import createSchema from "part:@sanity/base/schema-creator";
 
 // Then import schema types from any plugins that might expose them
-import schemaTypes from 'all:part:@sanity/base/schema-type'
+import schemaTypes from "all:part:@sanity/base/schema-type";
 
-import pageInfo from './pageInfo'
+import pageInfo from "./pageInfo";
+import experience from "./experience";
+import project from "./project";
+import skill from "./skill";
+import social from "./social";
 
 // Then we give our schema to the builder and provide the result to Sanity
 export default createSchema({
-  name: 'default',
-  types: schemaTypes.concat([
-    pageInfo,
-  ]),
-})
+  name: "default",
+  types: schemaTypes.concat([pageInfo, experience, project, skill, social]),
+});
