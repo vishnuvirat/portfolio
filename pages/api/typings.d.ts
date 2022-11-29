4cfd58d9dd5b7df01d64d3df9706752b2e06369e
Vishnu Chiluveri <vishnuviratkohli5@gmail.com>
2022-11-29 21:57:44 +0530

    Type definition completed

--- pages/api/typings.d.ts
+++ pages/api/typings.d.ts
@@ -12,3 +12,58 @@ interface Image {
     _type:'reference';
   };
 }
+
+
+export interface PageInfo extends SanityBody {
+  _type:'pageInfo';
+  address: string;
+  backgroundInformation:string;
+  email:string;
+  role:string;
+  heroImage:Image;
+  name:string;
+  phoneNumber:string;
+  profilePic:Image;
+}
+export interface Technology extends SanityBody {
+  _type:'skill';
+  title:string;
+  image:Image;
+  progress:number;
+}
+
+export interface Skill extends SanityBody {
+  _type:'skill';
+  title:string;
+  image:Image;
+  progress:number;
+}
+
+export interface Project extends SanityBody {
+  _type:'project';
+  title:string;
+  image:Image;
+  linkToBuild:string;
+  summary:string;
+  technologies:Technology[];
+}
+
+export interface Experience extends SanityBody {
+  _type:'experience';
+  company:string;
+  companyImage:Image;
+  datestarted:date;
+  dateEnded:date;
+  isCurrentlyWorkingHere:boolean;
+  jobTitle:string;
+  points: string[];
+  technologies:Technology[];
+}
+
+
+
+export interface Socials extends SanityBody {
+  _type:'social';
+  title:string;
+  url:string;
+}
