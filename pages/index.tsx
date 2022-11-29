2a03ed93218c98d7354b16143033aab4069a908a
Vishnu Chiluveri <vishnuviratkohli5@gmail.com>
2022-11-30 00:46:23 +0530

    updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,19 +8,29 @@ import Projects from "../components/Projects";
 import ContactMe from "../components/ContactMe";
 import Image from "next/image";
 import Link from "next/link";
-import {PageInfo, Experience, Project, Skill, Socials} from '../pages/api/typings'
-
-
+import {
+  PageInfo,
+  Experience,
+  Project,
+  Skill,
+  Socials,
+} from "../pages/api/typings";
+import { fetchPageInfo } from "../utils/fetchPageInfo";
+import { fetchExperience } from "../utils/fetchExperience";
+import { fetchSkills } from "../utils/fetchSkills";
+import { fetchProjects } from "../utils/fetchProjects";
+import { fetchSocials } from "../utils/fetchSocials";
+import type { GetStaticProps } from "next";
 
 type Props = {
   pageInfo: PageInfo;
-  experiences:Experience[];
-  skills:Skill[];
-  projects:Project[];
-  socials:Socials[];
-}
+  experiences: Experience[];
+  skills: Skill[];
+  projects: Project[];
+  socials: Socials[];
+};
 
-export default function Home() {
+const Home = ({ pageInfo, experiences, skills, projects, socials }: Props) => {
   return (
     <div className="bg-[rgb(36,36,36)] h-screen text-white snap-y snap-mandatory overflow-y-scroll overflow-x-hidden z-0 scrollbar scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
       <Head>
@@ -46,13 +56,39 @@ export default function Home() {
         <ContactMe />
       </div>
 
-      <Link href='#hero'>
+      <Link href="#hero">
         <div className="sticky bottom-5 w-full cursor-pointer">
           <div className="flex items-center justify-center">
-            <Image className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer" src='/vishnu.jpg' alt='' width='100' height='100'/>
+            <Image
+              className="h-10 w-10 rounded-full filter grayscale hover:grayscale-0 cursor-pointer"
+              src="/vishnu.jpg"
+              alt=""
+              width="100"
+              height="100"
+            />
           </div>
         </div>
       </Link>
     </div>
   );
-}
+};
+
+export default Home;
+
+export const getStaticProps: GetStaticProps<Props> = async () => {
+  const pageInfo: PageInfo = await fetchPageInfo();
+  const experiences: Experience[] = await fetchExperience();
+  const skills: Skill[] = await fetchSkills();
+  const projects: Project[] = await fetchProjects();
+  const socials: Socials[] = await fetchSocials();
+
+  return {
+    props: {
+      pageInfo,
+      experiences,
+      skills,
+      projects,
+      socials,
+    },
+  };
+};
--- utils/fetchProjects.ts
+++ utils/fetchProjects.ts
@@ -1,6 +1,6 @@
 import { Project } from "../pages/api/typings";
 
-export const fetchSocials = async() => {
+export const fetchProjects = async() => {
   const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/getProjects`);
 
   const data = await res.json();
