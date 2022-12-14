8b58cd333a6600d7e6562caba2dd94931870551b
Vishnu Chiluveri <vishnuviratkohli5@gmail.com>
2022-12-14 23:19:26 +0530

    changes

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -21,7 +21,7 @@ const Projects = ({ projects }: Props) => {
         </div>
         <div className="relative w-full flex overflow-x-scroll overflow-y-hidden snap-x snap-mandatory z-20 scrollbar scrollbar-thin scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80">
           {projects?.map((project, i) => (
-            <div key={} className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
+            <div key={project._id} className="w-screen flex-shrink-0 snap-center flex flex-col space-y-5 items-center justify-center p-20 md:p-44 h-screen">
               <motion.div
                 initial={{ y: -300, opacity: 0 }}
                 whileInView={{ opacity: 1, y: 0 }}
